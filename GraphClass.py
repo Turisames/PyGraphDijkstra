7fec79cfae152ac72f99c11c456d2d10bc8e34c6
Ville Nyrhilä <ville.nyrhila@student.tut.fi>
2016-09-29 19:31:57 +0300

    Moved the function createNodesFromFile from program.py to GraphClass.

--- GraphClass.py
+++ GraphClass.py
@@ -2,3 +2,14 @@
 class Graph:
     def __init__(self):
         self.__nodes = []
+
+    def __createNodesFromFile__(fileName = None):
+        if fileName == None:
+            fileName = getFileName()
+        file = open(fileName, 'r', encoding="utf-8")
+        lines = []
+        for line in file
+            line = line.strip()
+            if line != "" or line[0] != "#":
+                lines.append(line)
+        file.close()
--- program.py
+++ program.py
@@ -19,16 +19,5 @@ def main():
 def getFileName():
     return input("Give the name of the file you want me to read. ")
 
-def createNodesFromFile(fileName = None):
-    if fileName == None:
-        fileName = getFileName()
-    file = open(fileName, 'r', encoding="utf-8")
-    lines = []
-    for line in file
-        line = line.strip()
-        if line != "" or line[0] != "#":
-            lines.append(line)
-    file.close()
-
 
 main()
