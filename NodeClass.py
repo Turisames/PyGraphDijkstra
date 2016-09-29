9224e5b22ffe0cf643282e85fb2b177c626df2cc
Ville Nyrhilä <ville.nyrhila@student.tut.fi>
2016-09-29 18:46:03 +0300

    Added addNeighbour and setFromString to NodeClass.

--- NodeClass.py
+++ NodeClass.py
@@ -4,3 +4,18 @@ class Node:
         self.__name = Name
         self.__distance = None
         self.__previous = None
+
+    # Preconditions:
+    # Neighbour is a string, distance is an integer.
+    def __addNeighbour(self, Neighbour, distance):
+        self.__neighbours.append( (Neighbour, distance) )
+
+    def __setFromString(self, String):
+        # Format:
+        # Name;Neighbour1Name,Neighbour1Dist,...,NeighbourNName,NeighbourNDist
+        name, neighbours = String.split(";")
+        self.__name = name
+        neighbours = neighbours.split(",")
+
+        for element in neighbours:
+            add
