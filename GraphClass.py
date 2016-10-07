3e19589c8794c370483d60b3c3c5150e15c9b597
Ville Nyrhilä <ville.nyrhila@student.tut.fi>
2016-10-07 14:57:15 +0300

    Put in some code describing the Dijkstra algorithm.

--- GraphClass.py
+++ GraphClass.py
@@ -13,5 +13,19 @@ class Graph:
                 self.__nodes.append(nc.Node().__setFromString__(line))
         file.close()
 
-    def __figureRoute__(self, Start, Finish):
+    def __figureRoute__(self, Start = nc.Node, Finish = nc.Node):
         pass
+        current = Start
+        '''For the current node, consider all of its unvisited neighbors
+        and calculate their tentative distances.
+        Compare the newly calculated tentative distance to the current assigned value
+        and assign the smaller one.
+        For example, if the current node A is marked with a distance of 6,
+        and the edge connecting it with a neighbor B has length 2,
+        then the distance to B (through A) will be 6 + 2 = 8.
+        If B was previously marked with a distance greater than 8
+        then change it to 8.
+        Otherwise, keep the current value.'''
+        for neighbour in current.__neighbours__():
+            neighbour[]
+
--- NodeClass.py
+++ NodeClass.py
@@ -2,14 +2,14 @@ class Node:
     def __init__(self):
         self.__neighbours = []
         self.__name = ""
-        self.__weight = None
         self.__visited = False
+        self.__distance = None
         self.__previous = None
 
     # Preconditions:
     # Neighbour is a string, distance is an integer.
     def __addNeighbour__(self, Neighbour, weight):
-        self.__neighbours.append((Neighbour, weight))
+        self.__neighbours.append( (Neighbour, weight) )
 
     def __setFromString__(self, String):
         # Format:
@@ -21,5 +21,12 @@ class Node:
         for i in range(len(neighbours)):
             if i % 2 == 0:
                 self.__addNeighbour( neighbours[i],int(neighbours[i+1]) )
+
     def __setVisited__(self, newState):
         self.__visited = newState
+
+    def __setDistance__(self, Distance):
+        self.__distance = Distance
+
+    def __neighbours__(self):
+        return self.__neighbours
