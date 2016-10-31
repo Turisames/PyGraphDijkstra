31e78597662409c1d4ed5ddaaf1fcf168647e0b3
Ville Nyrhilä <ville.nyrhila@student.tut.fi>
2016-10-31 13:15:56 +0200

    I put in list that the algorithm uses to go to nodes that are closer to the source than to the node it is in at the moment. What is needed now is a way for the algorithm to remember, whence it came from. Also, I moved some things to their own methods for clarity's sake.

--- GraphClass.py
+++ GraphClass.py
@@ -29,18 +29,7 @@ class Graph:
             current.__setNext__( None )
             current.__setDistance__( None )
 
-    def __algo__(self, current, dist, Finish = nc.Node):
-        '''For the current node, consider all of its unvisited neighbors
-        and calculate their tentative distances.
-        '''
-        if type(current) is str:
-            current = self.__nodes[ current ]
-            current.__setDistance__( 0 ) # First round.
-
-        # Initiating the variable 'closest'
-        closest = None
-        print("########", current.__name__(), "########")
-
+    def __checkNeighbours__(self, current, dist, closest, Finish):
         for neighbourTuple in current.__neighbours__():
 
             # The neighbours are stored as Tuple.
@@ -62,10 +51,11 @@ class Graph:
                     closest = neighbour
 
                 # If we can see the Finish now, move onto it right away!
-                '''if neighbour.__name__() == Finish:
+                # Then again, as an optimizing procedure it's rather insignificant
+                if neighbour.__name__() == Finish:
                     closest = neighbour
                     break
-                '''
+
                 # Otherwise, keep looking.
                 if neighbour.__distance__() > ( dist + neighbourTuple[1] ):
                     '''then the distance to B (through A) will be 6 + 2 = 8.
@@ -74,25 +64,67 @@ class Graph:
                     Otherwise, keep the current value.'''
                     neighbour.__setDistance__( dist + neighbourTuple[1] )
                 print( neighbour.__name__(), neighbour.__distance__() )
+        return current, dist, closest
+
+    def __pickNextNode__(self, closest):
+        # The actual following node should be the one closest to the source.
+            for nodekey in self.__nodes:
+                node = self.__nodes[ nodekey ]
+
+                # The node has not been visited
+                if not node.__visited__():
+                    # The distance is not infinite and its distance is smaller
+                    # than the one that's closest to the current node.
+                    if node.__distance__() != None and \
+                                    node.__distance__() < closest.__distance__():
+                        closest = node
+            return closest
+
+    def __algo__(self, current, dist, Finish = nc.Node):
+        '''For the current node, consider all of its unvisited neighbors
+        and calculate their tentative distances.
+        '''
+        # This one only goes for the first call of this function, in practice.
+        if type(current) is str:
+            current = self.__nodes[ current ]
+            current.__setDistance__( 0 ) # First round.
+
+        ''' Initiating the variable "closest"
+            At the start of the function, the variable closest means
+            "closest to current node"
+        '''
+        closest = None
+
+        current, dist, closest = self.__checkNeighbours__( current, dist, closest, Finish )
 
         '''When we are done considering all of the neighbors of the current node,
         mark the current node as visited and remove it from the unvisited set.'''
         current.__setVisited__( True )
-
-        current.__setNext__( closest )
-
+        '''
         if self.__nodes[ Finish ].__visited__():
             # The algorithm was a success.
             return True
-
+        '''
         # This is where we go to the next node.
         if closest != None:
             # -- as we move on to the next one.
-            current = closest
-            # The distance will change.
-            #dist += closest.__distance__()
-            dist = current.__distance__()
-            return self.__algo__(current, dist, Finish)
+            current.__setNext__( closest )
+
+            ''' From this line on, closest refers to
+            "closest to source"'''
+            closest = self.__pickNextNode__( closest )
+
+            # The search has to continue
+            if not self.__nodes[ Finish ].__visited__():
+
+                current = closest
+                # The distance will change.
+                dist = current.__distance__()
+                return self.__algo__(current, dist, Finish)
+
+            # We've reached the right one.
+            elif self.__nodes[ Finish ].__visited__():
+                return True
         # This is where we get to, if no reasonable route could be found.
         else:
             return False
@@ -115,8 +147,9 @@ class Graph:
         if Result == False:
             # The whole thing failed.
             print("No route could be found.\n")
-        else:
+        elif Result == True:
             current = self.__nodes[ Start ]
+            print("The route is:")
             while current.__name__() != Finish:
                 print( current.__name__() + " " + str(current.__distance__()) )
                 current = current.__next__()
